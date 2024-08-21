fccc21d016b2f28aaa7a4a649efaad6ff03db6a3
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2024-08-21 17:42:49 +0700

    topics and test file

--- test/api_client_generator_test.dart
+++ test/api_client_generator_test.dart
@@ -1,1 +1,52 @@
+import 'package:test/test.dart';
+import 'package:api_client_generator/api_client_generator.dart';
+import 'dart:io';
 
+void main() {
+  group('ClientGenerator Tests', () {
+    test('should generate API client code successfully', () async {
+      final schemaPath = 'test/test_resources/sample_schema.yaml';
+      final outputPath = 'test_output';
+
+      final outputDir = Directory(outputPath);
+      if (outputDir.existsSync()) {
+        outputDir.deleteSync(recursive: true);
+      }
+
+      final schemaFile = File(schemaPath);
+      schemaFile.writeAsStringSync('''
+      paths:
+        /users:
+          get:
+            operationId: getUsers
+            responses:
+              '200':
+                description: successful operation
+      ''');
+
+      final generator = ClientGenerator(schemaPath, outputPath);
+
+      await generator.generate();
+
+      final generatedFile = File('$outputPath/api_client.dart');
+      expect(generatedFile.existsSync(), isTrue);
+      final generatedCode = generatedFile.readAsStringSync();
+      expect(generatedCode, contains('class ApiClient'));
+      expect(
+          generatedCode, contains('Future<Map<String, dynamic>> getUsers()'));
+
+      outputDir.deleteSync(recursive: true);
+      schemaFile.deleteSync();
+    });
+
+    test('should throw an error if schema file is missing', () async {
+      final schemaPath = 'invalid_path/schema.yaml';
+      final outputPath = 'test_output';
+
+      final generator = ClientGenerator(schemaPath, outputPath);
+
+      expect(() async => await generator.generate(),
+          throwsA(isA<FileSystemException>()));
+    });
+  });
+}
